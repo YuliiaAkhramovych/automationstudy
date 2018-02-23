25b2f7b825fff3c797936d18027bd72d7b94b571
Yuliia Akhramovych <Yuliia_Akhramovych@epam.com>
2018-02-23 16:29:18 +0200

    Completed 5th Linq task - "Int 50 problem"

--- GlobalProject/GlobalProject/Program.cs
+++ GlobalProject/GlobalProject/Program.cs
@@ -10,20 +10,30 @@ namespace GlobalProject
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            var list = new List<int>();
 
-            char[] symbols = new Char[] { ' ', '.', ',', ':', ';', '(', ')', '[', ']', '“', '‘', '/', '!', '?' };
 
-            var shortWords = Console.ReadLine()
-                .Split(symbols)
-                .Where(w => w != string.Empty)
-                .Where(w => w.Length < 5)
-                .OrderBy(word => word)
-                .Select(a => a.ToLower()).Distinct();
+            for (int i = 0; i <= 100; i++)
+            {
+                list.Add(random.Next(100));
 
-            Console.WriteLine(string.Join(", ", shortWords));
+            }
+
+            var sortedNumbers = list.OrderBy(n => n).ToList();
+            Console.WriteLine($"Sorted numbers in your collection are: {string.Join(", ", sortedNumbers)}");
+
+
+            if (list.Contains(50))
+            {
+                Console.WriteLine("There is number 50 in your collection");
+            }
+            else
+            {
+                Console.WriteLine("There is no number 50 in your collection");
+            }
 
             Console.ReadLine();
-
         }
     }
 }
